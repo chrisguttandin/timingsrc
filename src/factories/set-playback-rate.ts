6fcbed8afca42d0aa6076f4312a62b15a524808f
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-07-03 03:20:23 +0200

    fix: do not set the playbackRate to 1 again

--- src/factories/set-playback-rate.ts
+++ src/factories/set-playback-rate.ts
@@ -9,7 +9,8 @@ export const createSetPlaybackRate: TSetPlaybackRateFactory = (playbackRateAssig
             playbackRateAssignment[0] !== previousValue ||
             playbackRateAssignment[1] !== nextValue
         ) {
-            mediaElement.playbackRate = nextValue;
+            // There is currently a bug in Firefox which causes problems when switching back to a playbackRate of exactly 1.
+            mediaElement.playbackRate = nextValue === 1 ? (previousValue > 1 ? 1.00001 : 0.99999) : nextValue;
 
             playbackRateAssignments.set(mediaElement, [mediaElement.playbackRate, nextValue]);
         }
