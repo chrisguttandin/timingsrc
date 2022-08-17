36549e266ea2559a54578700c7b2e35573f2042c
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-08-17 16:36:53 +0200

    fix: add a function to check for supported playbackRate values

---
+++ src/functions/determine-supported-playback-rate-values.ts
@@ -0,0 +1,26 @@
+const DEFAULT_VALUES = [Number.MIN_VALUE, Number.MAX_VALUE];
+
+export const determineSupportedPlaybackRateValues = (window: null | (Window & typeof globalThis)) => {
+    if (window === null) {
+        return DEFAULT_VALUES;
+    }
+
+    const audio = new window.Audio();
+
+    try {
+        // Bug #2: Chrome does not support values above 16.
+        audio.playbackRate = 17;
+    } catch {
+        // Bug #1: Chrome does not support values below 0.625.
+        return [0.625, 16];
+    }
+
+    try {
+        // Bug #3: Firefox does not support negative values.
+        audio.playbackRate = -1;
+    } catch {
+        return [0, DEFAULT_VALUES[1]];
+    }
+
+    return DEFAULT_VALUES;
+};
