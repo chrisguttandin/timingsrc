53b2bd898c59575a9d10c284316487019fcd16c8
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-08-28 15:40:42 +0200

    test: wait for Safari to fire canplaythrough event

--- test/expectation/safari/media-element.js
+++ test/expectation/safari/media-element.js
@@ -35,19 +35,22 @@ describe('MediaElement', () => {
         // bug #5
 
         it('should limit the precision after a while', (done) => {
-            audioElement.currentTime = 0.4003200000000007;
+            audioElement.oncanplaythrough = () => {
+                audioElement.currentTime = 0.4003200000000007;
+                audioElement.oncanplaythrough = null;
 
-            expect(audioElement.currentTime).to.equal(0.4003200000000007);
-
-            setTimeout(() => {
                 expect(audioElement.currentTime).to.equal(0.4003200000000007);
 
                 setTimeout(() => {
-                    expect(audioElement.currentTime).to.equal(0.40032);
+                    expect(audioElement.currentTime).to.equal(0.4003200000000007);
 
-                    done();
-                }, 100);
-            });
+                    setTimeout(() => {
+                        expect(audioElement.currentTime).to.equal(0.40032);
+
+                        done();
+                    }, 100);
+                });
+            };
         });
     });
 });
