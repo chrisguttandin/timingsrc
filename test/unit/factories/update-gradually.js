f54a56d45a0c9697687e7990208076e0696cff25
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-09-21 15:20:07 +0200

    test: add tests for velocity values outside of the supported range

--- test/unit/factories/update-gradually.js
+++ test/unit/factories/update-gradually.js
@@ -1,17 +1,21 @@
 import { createUpdateGradually } from '../../../src/factories/update-gradually';
 
 describe('updateGradually()', () => {
+    let maximumValue;
+    let minimumValue;
     let timeConstant;
     let threshold;
     let tolerance;
     let updateGradually;
 
     beforeEach(() => {
+        maximumValue = 2;
+        minimumValue = 0.2;
         timeConstant = 1.5;
         threshold = 2;
         tolerance = 1;
 
-        updateGradually = createUpdateGradually([0, Number.MAX_VALUE], timeConstant, threshold, tolerance);
+        updateGradually = createUpdateGradually([minimumValue, maximumValue], timeConstant, threshold, tolerance);
     });
 
     describe('with a velocity below zero', () => {
@@ -186,7 +190,87 @@ describe('updateGradually()', () => {
         });
     });
 
-    describe('with a velocity above zero', () => {
+    describe('with a velocity below the minimum value', () => {
+        let velocity;
+
+        beforeEach(() => {
+            velocity = 0.1;
+        });
+
+        describe('with a position below zero', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: -2, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: -2, velocity: 0 });
+            });
+        });
+
+        describe('with a position below the threshold', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 2, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 2, velocity: 0 });
+            });
+        });
+
+        describe('with a position below the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 3, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 3, velocity: 0 });
+            });
+        });
+
+        describe('with a position within the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 4, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 4, velocity: 0 });
+            });
+        });
+
+        describe('with a position above the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 7, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 7, velocity: 0 });
+            });
+        });
+
+        describe('with a position above the threshold', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 8, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 8, velocity: 0 });
+            });
+        });
+    });
+
+    describe('with a velocity within the supported range of playbackRate values', () => {
         let velocity;
 
         beforeEach(() => {
@@ -217,15 +301,15 @@ describe('updateGradually()', () => {
             });
         });
 
-        describe('with a position below the tolerance that would result in a negative velocity', () => {
+        describe('with a position below the tolerance that would result in a velocity below the minimum value', () => {
             let timingStateVector;
 
             beforeEach(() => {
                 timingStateVector = { position: 3, velocity };
             });
 
-            it('should return an unchanged position but a velocity of zero', () => {
-                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 5, velocity: 0 });
+            it('should return an updated position and velocity', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 5, velocity: minimumValue });
             });
         });
 
@@ -233,11 +317,11 @@ describe('updateGradually()', () => {
             let timingStateVector;
 
             beforeEach(() => {
-                timingStateVector = { position: 3.75, velocity };
+                timingStateVector = { position: 3.875, velocity };
             });
 
             it('should return an updated position and velocity', () => {
-                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 5, velocity: 1 / 6 });
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 5, velocity: 3 / 12 });
             });
         });
 
@@ -257,11 +341,23 @@ describe('updateGradually()', () => {
             let timingStateVector;
 
             beforeEach(() => {
+                timingStateVector = { position: 6.25, velocity };
+            });
+
+            it('should return an updated position and velocity', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 5, velocity: 11 / 6 });
+            });
+        });
+
+        describe('with a position above the tolerance that would result in a velocity above the maximum value', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
                 timingStateVector = { position: 7, velocity };
             });
 
             it('should return an updated position and velocity', () => {
-                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 5, velocity: 7 / 3 });
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 5, velocity: maximumValue });
             });
         });
 
@@ -274,6 +370,86 @@ describe('updateGradually()', () => {
 
             it('should return an unchanged position and velocity', () => {
                 expect(updateGradually(timingStateVector, 5)).to.deep.equal(timingStateVector);
+            });
+        });
+    });
+
+    describe('with a velocity above the maximum value', () => {
+        let velocity;
+
+        beforeEach(() => {
+            velocity = 3;
+        });
+
+        describe('with a position below zero', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: -2, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: -2, velocity: 0 });
+            });
+        });
+
+        describe('with a position below the threshold', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 2, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 2, velocity: 0 });
+            });
+        });
+
+        describe('with a position below the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 3, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 3, velocity: 0 });
+            });
+        });
+
+        describe('with a position within the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 4, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 4, velocity: 0 });
+            });
+        });
+
+        describe('with a position above the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 7, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 7, velocity: 0 });
+            });
+        });
+
+        describe('with a position above the threshold', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 8, velocity };
+            });
+
+            it('should return an unchanged position but a velocity of zero', () => {
+                expect(updateGradually(timingStateVector, 5)).to.deep.equal({ position: 8, velocity: 0 });
             });
         });
     });
