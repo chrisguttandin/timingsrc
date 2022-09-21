2037950b92f25fad7553f60f4b0b02abb544a338
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-09-21 14:17:59 +0200

    refactor: remove redundant function parameters

--- src/factories/default-set-timingsrc.ts
+++ src/factories/default-set-timingsrc.ts
@@ -1,4 +1,4 @@
-import { TDefaultSetTimingsrcFactory, TSetTimingsrcFunction } from '../types';
+import { TDefaultSetTimingsrcFactory } from '../types';
 
 const DEFAULT_THRESHOLD = 1;
 const DEFAULT_TIME_CONSTANT = 0.5;
@@ -12,15 +12,14 @@ export const createDefaultSetTimingsrc: TDefaultSetTimingsrcFactory = (
     setTimingsrcWithCustomUpdateFunction,
     window
 ) =>
-    window !== null && window.navigator.userAgent.includes('Safari') && !window.navigator.userAgent.includes('Chrome')
-        ? (...args: Parameters<TSetTimingsrcFunction>) =>
-              createSetTimingsrc(setTimingsrcWithCustomUpdateFunction, createUpdateStepwise(DEFAULT_TOLERANCE))(...args)
-        : createSetTimingsrc(
-              setTimingsrcWithCustomUpdateFunction,
-              createUpdateGradually(
+    createSetTimingsrc(
+        setTimingsrcWithCustomUpdateFunction,
+        window !== null && window.navigator.userAgent.includes('Safari') && !window.navigator.userAgent.includes('Chrome')
+            ? createUpdateStepwise(DEFAULT_TOLERANCE)
+            : createUpdateGradually(
                   determineSupportedPlaybackRateValues(window),
                   DEFAULT_TIME_CONSTANT,
                   DEFAULT_THRESHOLD,
                   DEFAULT_TOLERANCE
               )
-          );
+    );
