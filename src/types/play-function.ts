dc7f32c05a369d3c2c26ec1269171ab4d81ecf32
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-09-21 20:59:16 +0200

    refactor: inline type definitions for the play() function

--- src/types/play-function.ts
+++
@@ -1,1 +0,0 @@
-export type TPlayFunction = (mediaElement: HTMLMediaElement) => void;
--- src/types/update-media-element-factory.ts
+++ src/types/update-media-element-factory.ts
@@ -1,12 +1,12 @@
+import type { play as playFunction } from '../functions/play';
 import { TPauseFunction } from './pause-function';
-import { TPlayFunction } from './play-function';
 import { TSetCurrentTimeFunction } from './set-current-time-function';
 import { TSetPlaybackRateFunction } from './set-playback-rate-function';
 import { TUpdateMediaElementFunction } from './update-media-element-function';
 
 export type TUpdateMediaElementFactory = (
     pause: TPauseFunction,
-    play: TPlayFunction,
+    play: typeof playFunction,
     setCurrentTime: TSetCurrentTimeFunction,
     setPlaybackRate: TSetPlaybackRateFunction
 ) => TUpdateMediaElementFunction;
