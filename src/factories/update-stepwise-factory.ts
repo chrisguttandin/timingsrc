4e7c600fe9d80f4aaad9998f060a7dfffe5030f3
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-09-22 12:39:37 +0200

    fix: handle multiple backwards jumps in Safari

--- src/factories/update-stepwise-factory.ts
+++ src/factories/update-stepwise-factory.ts
@@ -2,58 +2,151 @@ import type { TTranslateTimingStateVectorFunction } from 'timing-object';
 import { IUpdateVector } from '../interfaces';
 import { TUpdateFunction } from '../types';
 
+const MAXIMUM_PLAYHEAD_DIFFERENCE = 0.5;
+
 export const createUpdateStepwiseFactory = (translateTimingStateVector: TTranslateTimingStateVectorFunction) => {
-    return (tolerance: number): TUpdateFunction<IUpdateVector> => {
-        let lastMotionUpdate: null | { position: number; timestamp: number; velocity: number } = null;
-        let lastPlayheadDifference = 0;
-        let mediaElementDelay = 0;
+    return (
+        tolerance: number
+    ): TUpdateFunction<
+        IUpdateVector & {
+            lastAppliedPostion: number;
+            lastAppliedTimestamp: number;
+            lastAppliedVelocity: number;
+            lastPlayheadDifference: number;
+            mediaElementDelay: number;
+            numberOfDetectedResets: number;
+            numberOfExpectedResets: number;
+        }
+    > => {
+        return (timingStateVector, currentTime, previousUpdateVectorWithCustomState) => {
+            let {
+                lastAppliedPostion,
+                lastAppliedTimestamp,
+                lastAppliedVelocity,
+                lastPlayheadDifference,
+                mediaElementDelay,
+                numberOfDetectedResets,
+                numberOfExpectedResets
+            } = previousUpdateVectorWithCustomState ?? {
+                lastAppliedPostion: 0,
+                lastAppliedTimestamp: 0,
+                lastAppliedVelocity: 0,
+                lastPlayheadDifference: 0,
+                mediaElementDelay: 0,
+                numberOfDetectedResets: 0,
+                numberOfExpectedResets: 1
+            };
 
-        return (timingStateVector, currentTime) => {
             if (timingStateVector.position < 0 || timingStateVector.velocity === 0) {
-                lastMotionUpdate = null;
+                lastAppliedPostion = timingStateVector.position;
+                lastAppliedVelocity = timingStateVector.velocity;
 
-                return { position: timingStateVector.position, velocity: timingStateVector.velocity };
+                return {
+                    lastAppliedPostion,
+                    lastAppliedTimestamp: 0,
+                    lastAppliedVelocity,
+                    lastPlayheadDifference,
+                    mediaElementDelay,
+                    numberOfDetectedResets,
+                    numberOfExpectedResets,
+                    position: lastAppliedPostion,
+                    velocity: lastAppliedVelocity
+                };
             }
 
-            if (lastMotionUpdate !== null) {
-                const playheadDifference = Math.abs(currentTime - lastMotionUpdate.position);
+            // Bug #4: Safari decreases currentTime after playing for about 200 milliseconds.
+            if (lastAppliedVelocity === timingStateVector.velocity && lastPlayheadDifference < MAXIMUM_PLAYHEAD_DIFFERENCE) {
+                const playheadDifference = Math.abs(currentTime - lastAppliedPostion) * lastAppliedVelocity;
 
-                // Bug #4: Safari decreases currentTime after playing for about 200 milliseconds.
-                if (lastPlayheadDifference - 0.01 < playheadDifference && lastPlayheadDifference < 0.5) {
+                if (playheadDifference < MAXIMUM_PLAYHEAD_DIFFERENCE) {
+                    if (playheadDifference + 0.001 > lastPlayheadDifference) {
+                        lastPlayheadDifference = playheadDifference;
+
+                        if (numberOfDetectedResets < numberOfExpectedResets) {
+                            return {
+                                lastAppliedPostion,
+                                lastAppliedTimestamp,
+                                lastAppliedVelocity,
+                                lastPlayheadDifference,
+                                mediaElementDelay,
+                                numberOfDetectedResets,
+                                numberOfExpectedResets,
+                                position: currentTime,
+                                velocity: lastAppliedVelocity
+                            };
+                        }
+                    } else {
+                        lastPlayheadDifference = playheadDifference;
+                        numberOfDetectedResets += 1;
+
+                        if (numberOfDetectedResets <= numberOfExpectedResets) {
+                            return {
+                                lastAppliedPostion,
+                                lastAppliedTimestamp,
+                                lastAppliedVelocity,
+                                lastPlayheadDifference,
+                                mediaElementDelay,
+                                numberOfDetectedResets,
+                                numberOfExpectedResets,
+                                position: currentTime,
+                                velocity: lastAppliedVelocity
+                            };
+                        }
+
+                        numberOfExpectedResets += 1;
+                    }
+                } else {
                     lastPlayheadDifference = playheadDifference;
-
-                    return { position: currentTime, velocity: lastMotionUpdate.velocity };
+                    numberOfExpectedResets = Math.max(numberOfDetectedResets, 1);
                 }
-
-                lastPlayheadDifference = Number.POSITIVE_INFINITY;
+            } else {
+                lastAppliedTimestamp = 0;
             }
 
             const positionDifference = Math.abs(currentTime - timingStateVector.position);
+            const velocityHasChanged =
+                lastAppliedVelocity === 0 ||
+                (lastAppliedVelocity < 0 && timingStateVector.velocity > 0) ||
+                (lastAppliedVelocity > 0 && timingStateVector.velocity < 0);
 
-            if (positionDifference > tolerance) {
-                if (lastMotionUpdate !== null) {
-                    const elapsedTime = timingStateVector.timestamp - lastMotionUpdate.timestamp;
-
-                    const { position } = translateTimingStateVector({ acceleration: 0, ...lastMotionUpdate }, elapsedTime);
+            if (positionDifference > tolerance || velocityHasChanged) {
+                if (lastAppliedTimestamp > 0) {
+                    const elapsedTime = timingStateVector.timestamp - lastAppliedTimestamp;
+                    const { position } = translateTimingStateVector(
+                        { acceleration: 0, position: lastAppliedPostion, timestamp: lastAppliedTimestamp, velocity: lastAppliedVelocity },
+                        elapsedTime
+                    );
 
                     mediaElementDelay = position - currentTime;
                 }
 
-                const positioWithDelay = timingStateVector.position + mediaElementDelay;
+                lastAppliedPostion = timingStateVector.position + mediaElementDelay;
+                lastAppliedVelocity = timingStateVector.velocity;
 
-                lastMotionUpdate = {
-                    position: positioWithDelay,
-                    timestamp: timingStateVector.timestamp,
-                    velocity: timingStateVector.velocity
+                return {
+                    lastAppliedPostion,
+                    lastAppliedTimestamp: timingStateVector.timestamp,
+                    lastAppliedVelocity,
+                    lastPlayheadDifference: 0,
+                    mediaElementDelay,
+                    numberOfDetectedResets: 0,
+                    numberOfExpectedResets,
+                    position: lastAppliedPostion,
+                    velocity: lastAppliedVelocity
                 };
-                lastPlayheadDifference = 0;
-
-                return { position: positioWithDelay, velocity: timingStateVector.velocity };
             }
 
-            lastMotionUpdate = null;
-
-            return { position: currentTime, velocity: timingStateVector.velocity };
+            return {
+                lastAppliedPostion,
+                lastAppliedTimestamp,
+                lastAppliedVelocity,
+                lastPlayheadDifference,
+                mediaElementDelay,
+                numberOfDetectedResets,
+                numberOfExpectedResets,
+                position: currentTime,
+                velocity: timingStateVector.velocity
+            };
         };
     };
 };
---
+++ test/unit/factories/update-stepwise-factory.js
@@ -0,0 +1,259 @@
+import { createUpdateStepwiseFactory } from '../../../src/factories/update-stepwise-factory';
+import { stub } from 'sinon';
+
+describe('updateStepwise()', () => {
+    let defaultCustomState;
+    let tolerance;
+    let translateTimingStateVector;
+    let updateStepwise;
+
+    beforeEach(() => {
+        defaultCustomState = {
+            lastAppliedPostion: 0,
+            lastAppliedTimestamp: 0,
+            lastAppliedVelocity: 0,
+            lastPlayheadDifference: 0,
+            mediaElementDelay: 0,
+            numberOfDetectedResets: 0,
+            numberOfExpectedResets: 1
+        };
+        tolerance = 1;
+        translateTimingStateVector = stub();
+
+        updateStepwise = createUpdateStepwiseFactory(translateTimingStateVector)(tolerance);
+    });
+
+    describe('with a velocity below zero', () => {
+        let velocity;
+
+        beforeEach(() => {
+            velocity = -1;
+        });
+
+        describe('with a position below zero', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: -2, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: -2,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+
+        describe('with a position below the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 3, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: 3,
+                    lastAppliedTimestamp: 20,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+
+        describe('with a position within the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 4, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: 4,
+                    lastAppliedTimestamp: 20,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+
+        describe('with a position above the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 7, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: 7,
+                    lastAppliedTimestamp: 20,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+    });
+
+    describe('with a velocity of zero', () => {
+        let velocity;
+
+        beforeEach(() => {
+            velocity = 0;
+        });
+
+        describe('with a position below zero', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: -2, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: -2,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+
+        describe('with a position below the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 3, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: 3,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+
+        describe('with a position within the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 4, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: 4,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+
+        describe('with a position above the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 7, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: 7,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+    });
+
+    describe('with a velocity above zero', () => {
+        let velocity;
+
+        beforeEach(() => {
+            velocity = 1;
+        });
+
+        describe('with a position below zero', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: -2, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: -2,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+
+        describe('with a position below the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 3, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: 3,
+                    lastAppliedTimestamp: 20,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+
+        describe('with a position within the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 4, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: 4,
+                    lastAppliedTimestamp: 20,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+
+        describe('with a position above the tolerance', () => {
+            let timingStateVector;
+
+            beforeEach(() => {
+                timingStateVector = { position: 7, velocity };
+            });
+
+            it('should return an unchanged position and velocity', () => {
+                expect(updateStepwise({ ...timingStateVector, timestamp: 20 }, 5, null)).to.deep.equal({
+                    ...timingStateVector,
+                    ...defaultCustomState,
+                    lastAppliedPostion: 7,
+                    lastAppliedTimestamp: 20,
+                    lastAppliedVelocity: velocity
+                });
+            });
+        });
+    });
+});
