d18fa23afd74c11d1303e0d8dc4f359e14fd37c9
Christoph Guttandin <chrisguttandin@media-codings.com>
2026-04-24 20:13:11 +0200

    test(config): update Vitest config

--- config/vitest/expectation-safari-current.ts
+++ config/vitest/expectation-safari-current.ts
@@ -1,9 +1,9 @@
-import { env } from 'node:process';
 import { webdriverio } from '@vitest/browser-webdriverio';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        bail: 1,
         browser: {
             enabled: true,
             instances: [
